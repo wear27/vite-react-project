6aa4dd1b3adc44f0069cb07b79e50345f85926bb
wear27 <jwerpsn2033@gmail.com>
2024-11-13 19:42:49 +0300

    new hook

--- ReactCourse/src/App.jsx
+++ ReactCourse/src/App.jsx
@@ -10,37 +10,31 @@ import Header from './components/Header/Header'
 import JournalList from './components/JournalList/JournalList'
 import JournalAddButton from './components/JournalAddButton/JournalAddButton'
 import JournalForm from './components/JournalForm/JournalForm'
-import {useState} from 'react'
-import {useEffect} from 'react'
+//import {useState} from 'react'
+//import {useEffect} from 'react'
+import { useLocalStorage } from './hooks/use-localstorage.hook'
+
+function mapItems(items) {
+  if(!items) {
+    return []
+  }
+  return items.map(i => ({
+    ...i,
+    date: new Date(i.date)
+
+  }))
+}
 
 function App() {
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const data = JSON.parse(localStorage.getItem('data'));
-    if (data) {
-      setItems(data.map(item => ({
-        ...item,
-        date: new Date(item.date)
-      })));
-    }
-  }, []);
-
-  useEffect(() => {
-    if (items.length) {
-      console.log('Запись')
-      localStorage.setItem('data', JSON.stringify(items))
-    }
-    console.log(items);
-  }, [items]);
+  const [items, setItems] = useLocalStorage('data'); 
 
 
   const addItem = item => {
-    setItems(oldItems => [...oldItems,{
+    setItems([...mapItems(items), {
       text: item.text,
       title: item.title,
       date: new Date(item.date),
-      id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
+      id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
     }]);
   }
     
@@ -50,7 +44,7 @@ function App() {
       <LeftPanel>
         <Header/>
         <JournalAddButton/>
-        <JournalList items = {items} />
+        <JournalList items = {mapItems(items)}/>
       </LeftPanel>
 
       <Body>
